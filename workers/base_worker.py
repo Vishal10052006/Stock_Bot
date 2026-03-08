83938d28388175e92dda87db6fd0c09039f183e6
Vishal Singh <vishalsingh10052006@gmail.com>
2026-03-08 17:25:20 +0530

    base_worker_update-3_phase-3

--- workers/base_worker.py
+++ workers/base_worker.py
@@ -1,9 +1,9 @@
 class BaseWorker:
 
-    def __init__(self, name: str, capabilities: list, risk_level: str = "low"):
+    def __init__(self, name, capabilities, risk_level="low"):
         self.name = name
         self.capabilities = capabilities
         self.risk_level = risk_level
 
     async def execute(self, task: str):
-        raise NotImplementedError("Worker must implement execute()")
+        raise NotImplementedError
