c05da6d30d6f79cbdf928685ae1ddf16d74cc599
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-20 11:07:28 +0530

    feat(research): add Research Bot research/contracts/__init__.py

---
+++ research/contracts/__init__.py
@@ -0,0 +1,14 @@
+from .models import (
+    EventType,
+    ImpactResult,
+    ResearchContext,
+    ResearchDocument,
+    ResearchEvent,
+    ResearchSource,
+    SentimentResult,
+)
+
+__all__ = [
+    "EventType", "ImpactResult", "ResearchContext", "ResearchDocument",
+    "ResearchEvent", "ResearchSource", "SentimentResult",
+]
