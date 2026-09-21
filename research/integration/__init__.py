477a0edc0c1c92fff925fdd2f18487415f517011
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-21 13:42:28 +0530

    refactor(research): export completion contracts

---
+++ research/integration/__init__.py
@@ -0,0 +1,6 @@
+"""Research Bot integration contracts."""
+
+from research.integration.analysis_contract import ResearchAnalysisContext
+from research.integration.context import ResearchContextBuilder
+
+__all__ = ["ResearchAnalysisContext", "ResearchContextBuilder"]
