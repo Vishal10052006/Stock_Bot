8143ccfd244ece6d1e6e9b5504f9ac1684e7fe5a
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-22 12:03:51 +0530

    feat(analysis): export fundamental contracts and analyzer

--- intelligence/analysis/__init__.py
+++ intelligence/analysis/__init__.py
@@ -8,6 +8,17 @@ from intelligence.analysis.contracts import (
     AnalysisContractError,
 )
 from intelligence.analysis.engine import AnalysisEngine, analyze_latest_row
+from intelligence.analysis.fundamentals import (
+    FUNDAMENTAL_VERSION,
+    FundamentalContractError,
+    FundamentalSnapshot,
+    ValuationSnapshot,
+    FundamentalProvider,
+    InMemoryFundamentalProvider,
+    FundamentalAlignmentError,
+    align_fundamental_snapshot,
+    analyze_fundamentals,
+)
 
 __all__ = [
     "ANALYSIS_VERSION",
@@ -17,4 +28,13 @@ __all__ = [
     "AnalysisContractError",
     "AnalysisEngine",
     "analyze_latest_row",
+    "FUNDAMENTAL_VERSION",
+    "FundamentalContractError",
+    "FundamentalSnapshot",
+    "ValuationSnapshot",
+    "FundamentalProvider",
+    "InMemoryFundamentalProvider",
+    "FundamentalAlignmentError",
+    "align_fundamental_snapshot",
+    "analyze_fundamentals",
 ]
