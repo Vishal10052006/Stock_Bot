ef439aa4b3755759539e72ef5ef8ad26d03a7a43
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-23 08:49:01 +0530

    test(experiments): cover validation execution adapter

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,12 +1,15 @@
 """Reproducible research experiment contracts."""
 
 from .definition import ExperimentDefinition
+from .executor import ExperimentExecutionInputs, execute_validation_experiment
 from .record import ExperimentRecord
 from .runner import ExperimentExecution, ExperimentRunner
 
 __all__ = [
     "ExperimentDefinition",
     "ExperimentExecution",
+    "ExperimentExecutionInputs",
     "ExperimentRecord",
     "ExperimentRunner",
+    "execute_validation_experiment",
 ]
