873aa4126314fadb7fe6991d8398a82fa640ad12
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-23 08:43:29 +0530

    feat(experiments): add reproducible experiment records

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,5 +1,6 @@
 """Reproducible research experiment contracts."""
 
 from .definition import ExperimentDefinition
+from .record import ExperimentRecord
 
-__all__ = ["ExperimentDefinition"]
+__all__ = ["ExperimentDefinition", "ExperimentRecord"]
