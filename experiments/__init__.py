caa8f15f406ebb23f66e601b0403bbc4a900fe57
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-23 17:55:15 +0530

    feat(experiments): export S22 evaluation boundary

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,15 +1,18 @@
 """Reproducible research experiment contracts."""
 
 from .definition import ExperimentDefinition
+from .evaluation import EvaluationReport, evaluate_experiment_record
 from .executor import ExperimentExecutionInputs, execute_validation_experiment
 from .record import ExperimentRecord
 from .runner import ExperimentExecution, ExperimentRunner
 
 __all__ = [
+    "EvaluationReport",
     "ExperimentDefinition",
     "ExperimentExecution",
     "ExperimentExecutionInputs",
     "ExperimentRecord",
     "ExperimentRunner",
+    "evaluate_experiment_record",
     "execute_validation_experiment",
 ]
