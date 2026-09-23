7306265d5550beebbcd08512d1a45e9d508e99a2
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-24 00:32:02 +0530

    feat(prediction): expose inference monitoring and storage APIs

--- ml/prediction/__init__.py
+++ ml/prediction/__init__.py
@@ -2,9 +2,30 @@
 
 from .models import SignalModel, SignalPrediction
 from .monitoring import PredictionTelemetry
+from .contracts import (
+    ClassificationPrediction,
+    MultiHorizonForecast,
+    PredictionProvenance,
+    PredictionUncertainty,
+    ReturnForecast,
+)
+from .feature_drift import FeatureDriftReport, compare_feature_distributions
+from .inference import PredictionInferenceRequest, PredictionInferenceService
+from .storage import PredictionRecord, PredictionStore
 
 __all__ = [
     "SignalModel",
     "SignalPrediction",
     "PredictionTelemetry",
+    "ClassificationPrediction",
+    "MultiHorizonForecast",
+    "PredictionProvenance",
+    "PredictionUncertainty",
+    "ReturnForecast",
+    "FeatureDriftReport",
+    "compare_feature_distributions",
+    "PredictionInferenceRequest",
+    "PredictionInferenceService",
+    "PredictionRecord",
+    "PredictionStore",
 ]
