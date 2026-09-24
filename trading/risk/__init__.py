30ff6422ca3ae7ff59faa615ec3636450624d928
Vishal Raj <vishalsingh10052006@gmail.com>
2026-09-24 21:12:14 +0530

    feat(risk): export transition sizing contract

--- trading/risk/__init__.py
+++ trading/risk/__init__.py
@@ -8,6 +8,7 @@ from .contracts import (
     RiskAction,
     RiskCheck,
     RiskPositionContext,
+    RiskTransitionSizing,
     RiskPositionTransition,
     RiskReasonCode,
 )
@@ -31,6 +32,7 @@ __all__ = [
     "RiskEngine",
     "RiskInput",
     "RiskPositionContext",
+    "RiskTransitionSizing",
     "RiskPositionTransition",
     "RiskReasonCode",
     "build_target",
